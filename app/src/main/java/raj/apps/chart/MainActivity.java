dbd5b57516fa5e91ae5aa008c40fb1d14342b452
Rajakumar <raj.nivuz@gmail.com>
2020-01-24 11:41:41 +0530

    Library added to activity

--- app/src/main/java/raj/apps/chart/MainActivity.java
+++ app/src/main/java/raj/apps/chart/MainActivity.java
@@ -2,13 +2,28 @@ package raj.apps.chart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.lib.chart.PieChart;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        float d[]={10.0f,30f,55.0f,5.0f};
+        String n[]={"One","Two","Three","Four"};
+        int colors[]={Color.GREEN,Color.RED,Color.MAGENTA,Color.BLUE};
+        PieChart pieChart = findViewById(R.id.chart);
+        pieChart.setMapValues(d);
+        pieChart.setMapValueName(n);
+        pieChart.setColors(colors);
+        pieChart.show();
+
+
     }
 }
